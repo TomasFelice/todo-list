dfbb78d4f4d64959bf848aa782fded9158c330ed
Tomi Felice <tomasagustin.200358@gmail.com>
2024-03-13 23:00:34 -0300

    mark completed

--- index.php
+++ index.php
@@ -16,16 +16,39 @@
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
+
+        <script src="https://kit.fontawesome.com/b6d73066f0.js"
+                crossorigin="anonymous">
+        </script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous">
+        </script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous">
+        </script>
+        
+        <style>
+            .completed {
+                text-decoration: line-through;
+                opacity: 0.5;
+            }
+            </style>
     </head>
 
-    <body>
+    <body class="bg-dark">
         <header>
             <!-- place navbar here -->
         </header>
-        <main class="container">
-            <section class="card">
-                <header class="card-header">
-                    Lista de Tareas Interactiva
+        <main class="container mt-3">
+            <section class="card bg-light">
+                <header class="card-header d-flex justify-content-center">
+                    <h1>Lista de Tareas Interactiva</h1>
                 </header>
                 <main class="card-body">
                     <form action="" method="post">
@@ -52,7 +75,7 @@
                         </div>
                         <div class="form-floating">
                             <button 
-                                class="btn btn-outline-primary mt-3"
+                                class="btn btn-outline-success mt-3"
                                 name="add_task"
                                 id="add_task"
                                 type="submit">
@@ -66,20 +89,39 @@
                         <?php
                         foreach($tasks as $task){ ?>
                         <li class="list-group-item">
-                            <input class="form-check-input me-1" type="checkbox" value="" />
-                            <h6><?php echo $task['title']; ?></h6>
-                            <p><em><small><?php echo $task['description']; ?></small></em></p>
+                            <form action="" method="post">
+                                <input 
+                                    type="hidden" 
+                                    name="id" 
+                                    value="<?php echo $task['id']; ?>"
+                                >
+                                <input 
+                                    class="form-check-input me-1" 
+                                    type="checkbox" 
+                                    name="task_checkbox"
+                                    id="task_checkbox"
+                                    value="<?php echo $task['completed']; ?>"
+                                    onChange="this.form.submit()"
+                                    <?php echo ($task['completed'] == 1) ? 'checked': '';?> 
+                                >
+                            </form>
+                            
+                            <h6 class="<?php echo ($task['completed'] == 1) ? 'completed': '';?>">
+                                <?php echo $task['title']; ?>
+                            </h6>
+                            <p class="<?php echo ($task['completed'] == 1) ? 'completed': '';?>">
+                                <em><small><?php echo $task['description']; ?></small></em>
+                            </p>
                             <a
                                 class="btn btn-danger btn-sm float-end"
                                 name="delete_task"
                                 id="delete_task"
-                                href="?id=<?php echo $task['id']; ?>"
-                                >
+                                href="?id=<?php echo $task['id']; ?>">
                                 Eliminar
                                 <span
                                     class="badge bg-danger"
-                                    ><i class="fa-solid fa-trash"></i></span
-                                >
+                                    ><i class="fa-solid fa-trash"></i>
+                                </span>
                             </a>
                             <button
                                 class="btn btn-primary btn-sm float-end me-1">
@@ -99,24 +141,6 @@
                 </footer>
             </section>
         </main>
-        <footer>
-            <!-- place footer here -->
-        </footer>
-    
-        <script src="https://kit.fontawesome.com/b6d73066f0.js"
-                crossorigin="anonymous">
-        </script>
 
-        <script
-            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-            crossorigin="anonymous"
-        ></script>
-
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-            crossorigin="anonymous"
-        ></script>
     </body>
 </html>
--- taskController.php
+++ taskController.php
@@ -1,19 +1,39 @@
 <?php
 
+$dsn = "mysql:host=localhost:3306;dbname=todo_list_db";
+$username = "root";
+$password = "Chic@g01710!";
+
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=todo_list_db", "root", "");
+    $conn = new PDO($dsn, $username, $password);
 }catch(PDOException $e){
     echo "Error de conexión: " . $e->getMessage();
+}
+
+if(isset($_POST['id'])){
+    $id = $_POST['id'];
+    $completed = (isset($_POST['task_checkbox'])) ? 1 : 0;
+    $sql = "UPDATE task SET completed = ? WHERE id = ?";
+    $statement = $conn->prepare($sql);
+    $statement->execute([$completed, $id]);
 }
 
 if(isset($_POST['add_task'])){
     $taskTitle = ($_POST['taskTitle']);
     $taskDescription = ($_POST['taskDescription']);
-    $sqlInsert = "INSERT INTO task (title, description) VALUES (?, ?)";
-    $statement = $conn->prepare($sqlInsert);
+    $sql = "INSERT INTO task (title, description) VALUES (?, ?)";
+    $statement = $conn->prepare($sql);
     $statement->execute([$taskTitle, $taskDescription]);
 }
 
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "DELETE FROM task WHERE id = ?";
+    $statement = $conn->prepare($sql);
+    $statement->execute([$id]);
+    header("Location: index.php");
+}
 
 $sqlGetAll = "SELECT * FROM task";
 $tasks = $conn->query($sqlGetAll);
